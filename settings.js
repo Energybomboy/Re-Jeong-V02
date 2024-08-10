616885658037ea77befeb938f49071fba71a226b
Takudzwa-Mlambo <157640942+Cod3Uchiha@users.noreply.github.com>
2024-08-10 21:37:49 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,6 +2,7 @@ global.prefa = ['','!','.',',','🐤','🗿']
 
 global.owner = ['6281401795787']
 global.botname = 'Dutz'
+global.sessionName = 'session'
 global.baileys1 = require('@whiskeysockets/baileys') 
 global.sticker1 = "dutz"
 global.sticker2 = "🌜"
